e3ecdec85e508588bea2ca39a2d3c41aaf5521b8
idealist.eden@gmail.com <codebuilder.c7>
2017-06-28 19:35:20 +0800

    经纬度计算工具

---
+++ xhtc/src/main/java/GPSMethod.java
@@ -0,0 +1,101 @@
+/**
+ * Created by xhtc on 2017/6/28.
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 经纬度坐标计算方法
+ */
+public class GPSMethod {
+
+    private static double EARTH_RADIUS = 6378.137;
+
+    private static double rad(double d) {
+        return d * Math.PI / 180.0;
+    }
+
+    /**
+     * 根据两个位置的经纬度，来计算两地的距离（单位为KM）
+     * 参数为String类型
+     * @param lat1 原纬度
+     * @param lng1 原经度
+     * @param lat2 目标纬度
+     * @param lng2 目标经度
+     * @return
+     */
+    public static double getDistance(double lat1, double lng1, double lat2, double lng2) {
+
+        double radLat1 = rad(lat1);
+        double radLat2 = rad(lat2);
+        double difference = radLat1 - radLat2;
+        double mdifference = rad(lng1) - rad(lng2);
+        double distance = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(difference / 2), 2)
+                + Math.cos(radLat1) * Math.cos(radLat2)
+                * Math.pow(Math.sin(mdifference / 2), 2)));
+        distance = distance * EARTH_RADIUS;
+        distance = Math.round(distance * 10000) / 10000;
+
+        return distance;
+    }
+
+    /**
+     * 根据两个位置的经纬度，来计算两地的距离（单位为米）
+     * @param lat1
+     * @param lng1
+     * @param lat2
+     * @param lng2
+     * @return
+     */
+    public static double getDistanceInMeter(double lat1, double lng1, double lat2,
+                                            double lng2) {
+        double radLat1 = rad(lat1);
+        double radLat2 = rad(lat2);
+        double a = radLat1 - radLat2;
+        double b = rad(lng1) - rad(lng2);
+        double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2)
+                + Math.cos(radLat1) * Math.cos(radLat2)
+                * Math.pow(Math.sin(b / 2), 2)));
+        s = s * EARTH_RADIUS;
+        s = Math.round(s * 10000d) / 10000d;
+        s = s*1000;
+        return s;
+    }
+
+    /**
+     * 获取当前坐标一定距离以内的经纬度值,单位米
+     * 最小经度 minLng
+     * 最小纬度 maxLat
+     * 最大经度 maxLng
+     * 最大纬度 minLat
+     */
+    public static Map<String, Double> getAround(double lat, double lon, int raidus) {
+        Map<String, Double> map = new HashMap<String, Double>();
+
+        Double latitude = lat;
+        Double longitude = lon;
+
+        Double degree = (24901 * 1609) / 360.0;
+        double raidusMile = raidus;
+
+        Double dpmLat = 1 / degree;
+        Double radiusLat = dpmLat * raidusMile;
+        Double minLat = latitude - radiusLat;
+        Double maxLat = latitude + radiusLat;
+
+        Double mpdLng = degree * Math.cos(latitude * (Math.PI / 180));
+        Double dpmLng = 1 / mpdLng;
+        Double radiusLng = dpmLng * raidusMile;
+        Double minLng = longitude - radiusLng;
+        Double maxLng = longitude + radiusLng;
+
+        map.put("minLat", minLat);
+        map.put("maxLat", maxLat);
+        map.put("minLng", minLng);
+        map.put("maxLng", maxLng);
+
+        return map;
+    }
+
+}
