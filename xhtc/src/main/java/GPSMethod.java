6daa51af54fff9133f6765e7fa76196866d55e79
idealist.eden@gmail.com <codebuilder.c7>
2017-06-30 09:23:37 +0800

    GPS坐标转换  高德 -> 百度

--- xhtc/src/main/java/GPSMethod.java
+++ xhtc/src/main/java/GPSMethod.java
@@ -2,8 +2,16 @@
  * Created by xhtc on 2017/6/28.
  */
 
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+
+import static java.lang.Math.*;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
 
 /**
  * 经纬度坐标计算方法
@@ -96,6 +104,74 @@ public class GPSMethod {
         map.put("maxLng", maxLng);
 
         return map;
+    }
+
+    /**
+     * gcj02标准坐标系转换成bd09坐标系
+     * 高德 -> 百度
+     * @param gg_lat 源坐标纬度
+     * @param gg_lon 源坐标经度
+     * @return
+     */
+    public Pair<Double, Double> gcj02_to_bd09(Double gg_lat, Double gg_lon){
+        double x = gg_lon, y = gg_lat;
+        double z = sqrt(x * x + y * y) + 0.00002 * sin(y * Math.PI);
+        double theta = atan2(y, x) + 0.000003 * cos(x * Math.PI);
+        double bd_lon = z * cos(theta) + 0.0065;
+        double bd_lat = z * sin(theta) + 0.006;
+        return Pair.of(geoFormat(bd_lat), geoFormat(bd_lon));
+    }
+
+    /**
+     * bd09标准坐标系转换成gcj02坐标系
+     * 百度 -> 高德
+     * @param bd_lat 源坐标纬度
+     * @param bd_lon 源坐标经度
+     * @return
+     */
+    public Pair<Double, Double> bd09_to_gcj02(Double bd_lat, Double bd_lon){
+        double x = bd_lon - 0.0065, y = bd_lat - 0.006;
+        double z = sqrt(x * x + y * y) - 0.00002 * sin(y * Math.PI);
+        double theta = atan2(y, x) - 0.000003 * cos(x * Math.PI);
+        double gg_lon = z * cos(theta);
+        double gg_lat = z * sin(theta);
+        return Pair.of(geoFormat(gg_lat), geoFormat(gg_lon));
+    }
+
+    /**
+     * 经纬度转换成小数点后八位
+     *
+     * @param d
+     * @return
+     */
+    private double geoFormat(double d) {
+        BigDecimal bigDecimal = new BigDecimal(d);
+        return bigDecimal.setScale(8, BigDecimal.ROUND_CEILING).doubleValue();
+    }
+
+
+    /**
+     * 关键字：华星现代产业园F座 (经度,纬度)
+     * 高德地图坐标：GCJ-02标准 120.121663,30.274788
+     * 百度地图坐标：BD-09标准  120.127832,30.281228
+     *
+     * Point: x:经度 y:纬度
+     * 百度api给出的结果： x是经度，y是纬度
+     */
+    public static void main(String[] args) {
+        GPSMethod method = new GPSMethod();
+
+        BigDecimal bdLng = BigDecimal.valueOf(120.127832);
+        BigDecimal bdLat = BigDecimal.valueOf(30.281228);
+        BigDecimal gdLng = BigDecimal.valueOf(120.121663);
+        BigDecimal gdLat = BigDecimal.valueOf(30.274788);
+
+        Pair<Double, Double> pair = method.gcj02_to_bd09(gdLat.doubleValue(), gdLng.doubleValue());
+        System.out.println("经度:" + pair.getRight());
+        System.out.println("纬度" + pair.getLeft());
+        Double distance2 = getDistanceInMeter(pair.getLeft(), pair.getRight(), bdLat.doubleValue(), bdLng.doubleValue());
+        System.out.println("方法转换高德数据后的坐标和百度直接定位的距离误差(M)：" + distance2);
+
     }
 
 }
