8f0b442fdccbde2ea10772e54643718f97bbda52
rayeden <idealist.eden@gmail.com>
2017-07-07 10:47:38 +0800

    BigDecimal设置小数位数 向下取整操作（String） maven配置jdk编译版本1.7

--- xhtc/src/main/java/BigDecimalTest.java
+++ xhtc/src/main/java/BigDecimalTest.java
@@ -1,4 +1,6 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 /**
  * Created by BG289522 on 2017/7/7.
@@ -13,6 +15,13 @@ public class BigDecimalTest {
 
         //设置成两位小数，2.50
         System.out.println(num);
+
+        //转成两位小数的String
+        BigDecimal num1 = BigDecimal.valueOf(2.358);
+        DecimalFormat df = new DecimalFormat("0.00");
+        //向下取整
+        df.setRoundingMode(RoundingMode.FLOOR);
+        System.out.println(df.format(num1));
 
     }
 
