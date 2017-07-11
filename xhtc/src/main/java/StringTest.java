dd5a423e9f567ae8f42bdf928651f72b4d0ec632
rayeden <idealist.eden@gmail.com>
2017-07-11 21:42:40 +0800

    String.contains()

---
+++ xhtc/src/main/java/StringTest.java
@@ -0,0 +1,16 @@
+/**
+ * Created by BG289522 on 2017/7/11.
+ */
+public class StringTest {
+
+    public static void main(String[] args) {
+
+        String a = "上海市";
+
+        String b = "上海";
+
+        System.out.println(a.contains(b));
+
+        System.out.println(b.contains(a));
+    }
+}
