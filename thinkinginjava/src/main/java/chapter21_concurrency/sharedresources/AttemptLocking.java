581d1edabc74697fa78c6417936a0fe222dfd43d
rayeden <idealist.eden@gmail.com>
2017-08-07 21:41:39 +0800

    lock.tryLock() 自旋锁

---
+++ thinkinginjava/src/main/java/chapter21_concurrency/sharedresources/AttemptLocking.java
@@ -0,0 +1,58 @@
+package chapter21_concurrency.sharedresources;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by xhtc on 2017/8/7.
+ */
+public class AttemptLocking {
+
+    private ReentrantLock lock = new ReentrantLock();
+
+    public void untimed() {
+        boolean captured = lock.tryLock();
+        try {
+            System.out.println("tryLock: " + captured);
+        } finally {
+            if (captured)
+                lock.unlock();
+        }
+    }
+
+    public void timed() {
+        boolean captured = false;
+        try {
+            //尝试2秒获取锁，没获取就返回失败
+            captured = lock.tryLock(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            System.out.println("tryLock(2, TimeUnit.SECONDS): " + captured);
+        } finally {
+            if (captured)
+                lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        final AttemptLocking al = new AttemptLocking();
+        al.untimed();
+        al.timed();
+
+        new Thread() {
+            {
+                setDaemon(true);
+            }
+            public void run(){
+                al.lock.lock();
+                System.out.println("acquired");
+            }
+        }.start();
+
+        Thread.yield();
+        al.untimed();
+        al.timed();
+    }
+}
