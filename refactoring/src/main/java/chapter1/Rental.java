a29f1db625bfe2bcce2f1e473848bae3ba38e6df
rayeden <idealist.eden@gmail.com>
2017-09-22 21:42:29 +0800

    重构

---
+++ refactoring/src/main/java/chapter1/Rental.java
@@ -0,0 +1,32 @@
+package chapter1;
+
+/**
+ * Created by BG289522 on 2017/9/22.
+ */
+public class Rental {
+
+    private Movie _movie;
+
+    private int _daysRented;
+
+    public Rental(Movie _movie, int _daysRented) {
+        this._movie = _movie;
+        this._daysRented = _daysRented;
+    }
+
+    public Movie get_movie() {
+        return _movie;
+    }
+
+    public void set_movie(Movie _movie) {
+        this._movie = _movie;
+    }
+
+    public int get_daysRented() {
+        return _daysRented;
+    }
+
+    public void set_daysRented(int _daysRented) {
+        this._daysRented = _daysRented;
+    }
+}
